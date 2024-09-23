8c5662a6aaf8faec3ca1d49f470dd5a32e851adf
friedkaiju <61223378+friedkaiju@users.noreply.github.com>
2024-09-23 14:06:18 +1000

    Developed simplistic UI and re-wrote the game
    Utilised DOM manipulation techniques in place of loops, prompts and console logging. All console logs removed and replaced.
    
    Re-wrote winner declaration logic to instead depend on whether either player reaches 5 points, rather than looping through the function 5x.
    
    Committed to rps-ui pre-merging into main.

--- javascript.js
+++ javascript.js
@@ -1,85 +1,82 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getHumanChoice() {
-   let humanChoice = prompt("choose your weapon! (scissors, paper, rock)").toLowerCase();
-         if (humanChoice === "scissors") {
-            let humanChoice = "scissors"
-            console.log(`you chose ${humanChoice}!`);
-            return humanChoice
-        } else if (humanChoice === "paper") {
-            let humanChoice = "paper"
-            console.log(`you chose ${humanChoice}!`);
-            return humanChoice
-        } else if (humanChoice === "rock") {
-            let humanChoice = "rock"
-            console.log(`you chose ${humanChoice}!`);
-            return humanChoice
-        } else if (humanChoice != "scissors","paper","rock") {
-            alert(`the weapon you chose is ${humanChoice}, which is invalid! try again!`);
-            return getHumanChoice();
-        }
- };
+const scissors = document.querySelector("#scissors");
+const paper = document.querySelector("#paper");
+const rock = document.querySelector("#rock");
+const resultContainer = document.querySelector("#results")
+
+
+scissors.addEventListener("click", () => {
+    humanChoice = "scissors";
+    resultParagraph.textContent += `you chose ${humanChoice}! `
+    playRound("scissors", getComputerChoice());
+});
+paper.addEventListener("click", () => {
+    humanChoice = "paper";
+    resultParagraph.textContent += `you chose ${humanChoice}! `
+    playRound("paper", getComputerChoice());
+});
+rock.addEventListener("click", () => {
+    humanChoice = "rock";
+    resultParagraph.textContent += `you chose ${humanChoice}! `
+    playRound("rock", getComputerChoice());
+});
+/* add an event listener to the buttons that call the playround function with the correct player selection*/
+
+const resultParagraph = document.querySelector("#results");
+    const resultParagraphText = document.createElement("p")
+resultParagraph.appendChild(resultParagraphText);
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3).toString();
         if (computerChoice === "0") {
             let computerChoice = "scissors"
-            console.log(`computer chose ${computerChoice}!`);
+            resultParagraph.textContent += `computer chose ${computerChoice}! `;
             return computerChoice
         } else if (computerChoice === "1") {
             let computerChoice = "paper"
-            console.log(`computer chose ${computerChoice}!`);
+            resultParagraph.textContent += `computer chose ${computerChoice}! `;
             return computerChoice
         } else if (computerChoice === "2") {
             let computerChoice = "rock"
-            console.log(`computer chose ${computerChoice}!`);
+            resultParagraph.textContent += `computer chose ${computerChoice}! `;
             return computerChoice
         }
 };
 
-function playGame() {
-for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-    playRound (humanSelection, computerSelection);
+playRound (humanSelection, computerSelection);
 
-        function playRound(humanChoice, computerChoice)
-        {
-            if (humanChoice === computerChoice) {
-                console.log(`| its a tie! score is unchanged |`)
+    function playRound(humanChoice, computerChoice)
+{
+     if (humanScore === 5 || computerScore === 5) {
+                declareWinner();
+    } else if (humanChoice === computerChoice) {
+            resultParagraph.textContent += '| its a tie! score is unchanged | '
     } else if (humanChoice === "scissors" && computerChoice === "paper" ||
             humanChoice === "paper" && computerChoice === "rock" ||
             humanChoice === "rock" && computerChoice === "scissors") 
-            {   humanScore ++
-                console.log(`you win the round! ${humanChoice} beats ${computerChoice}`)
-                console.log(`your score: ${humanScore} | computer's score: ${computerScore}`)
+    {   humanScore ++
+            resultParagraph.textContent += `you win the round! ${humanChoice} beats ${computerChoice} `
+            resultParagraph.textContent += `your score: ${humanScore} | computer's score: ${computerScore} `
                 
     } else if (humanChoice === "scissors" && computerChoice === "rock" ||
             humanChoice === "paper" && computerChoice === "scissors" ||
             humanChoice === "rock" && computerChoice === "paper") 
-            { computerScore ++
-                console.log(`you lose the round! computer chose ${computerChoice} which beats ${humanChoice}`)
-                console.log(`your score: ${humanScore} | computer's score: ${computerScore}`)
-            }
+        { computerScore ++
+         resultParagraph.textContent += `you lose the round! computer chose ${computerChoice} which beats ${humanChoice} `
+        resultParagraph.textContent += `your score: ${humanScore} | computer's score: ${computerScore} `
         }
-    }
 };
 
-playGame ();
-
-function finalScore() {
-    if (humanScore === computerScore) {
-        console.log(`game is a draw! -_-`)
-        console.log(`final scores | your score: ${humanScore} | computer's score: ${computerScore}`)
-    } else if (humanScore > computerScore) {
-        console.log(`you win the game! :)`)
-        console.log(`final scores | your score: ${humanScore} | computer's score: ${computerScore}`)
-    } else if (humanScore < computerScore) {
-        console.log(`computer wins the game :(`)
-        console.log(`final scores | your score: ${humanScore} | computer's score: ${computerScore}`)
-    }
-};
-
-finalScore();
+function declareWinner() {
+    const winnerMessage = humanScore === 5 ? `you win the game! :) final scores | your score: ${humanScore} | computer's score: ${computerScore}` : `computer wins the game :( final scores | your score: ${humanScore} | computer's score: ${computerScore}`;
+    const winnerParagraph = document.createElement("p");
+    winnerParagraph.textContent = winnerMessage;
+    resultContainer.appendChild(winnerParagraph);
+}
+/*shorthand for if/else statements. if the first condition is true, it will send the first message and if not it will send the second "first" : "second"
+then of course create the paragraph, fill in the text content and append it to the parent node/container div*/
